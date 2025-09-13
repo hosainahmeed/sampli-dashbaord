1376f0eb81e0f57960be29a65accb6ee4b087ea0
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2025-09-13 19:56:15 +0600

    u

--- src/Redux/main/server.js
+++ src/Redux/main/server.js
@@ -1,1 +1,2 @@
-export const url = `http://10.10.20.9:3500` // Hosain ahmed server
+// export const url = `http://10.10.20.9:3500` // Hosain ahmed server
+export const url = `https://rnj64vmh-3500.inc1.devtunnels.ms` // Hosain ahmed server
