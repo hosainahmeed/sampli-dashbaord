46dac4085a9d368b146b61d53ce45312f3653bf5
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2025-09-16 12:02:04 +0600

    üpdate the des

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
-    host: "10.10.20.16",
+    host: "0.0.0.0",
     port: 5173,
   },
 });
