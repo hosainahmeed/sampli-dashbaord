1806676ba8099f3d27068e3a14ba5b53a7225fd0
hosainahmeed <skyfal430@gmail.com>
2025-12-25 17:35:21 +0600

    update shipmant layout update

--- src/pages/sampler/SamplerHome/components/shipments/ShipmentAndAlertSampler.jsx
+++ src/pages/sampler/SamplerHome/components/shipments/ShipmentAndAlertSampler.jsx
@@ -4,13 +4,13 @@ import SidebarShipmentsSampler from './shared/SidebarShipmentsSampler'
 
 const ShipmentAndAlertSampler = () => {
   return (
-    <div className="responsive-width">
-      <div className="p-4 box-border rounded-md flex justify-between items-start  h-screen">
+    <div className="container mx-auto">
+      <div className="box-border rounded-md flex justify-between items-start  h-screen">
         <div className="max-w-[280px] max-md:max-w-20  w-full rounded-md ">
           <SidebarShipmentsSampler />
         </div>
         <div className="w-full max-w-[calc(100vw-250px)] max-md:max-w-full scroll-y-auto    ">
-          <div className="h-[94vh] ">
+          <div className="h-[94vh]">
             <Outlet />
           </div>
         </div>
